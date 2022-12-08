20997e78cae476bc23f58a67ee7a37b57a0f792c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-08 19:20:29 +0300

    name usestate rendered

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import './App.css';
-import Greet from './component/Greet';
-import Welcome from './component/Welcome';
-import Hello from './component/Hello'
+import React from "react";
+import "./App.css";
+import Greet from "./component/Greet";
+import Welcome from "./component/Welcome";
+import Hello from "./component/Hello";
+import Title from "./component/Title";
+import Myname from "./component/Mycount";
 function App() {
   return (
     <div className="App">
-     <Greet/>
-     <Welcome/>
-     <Hello/>
+      <Greet />
+      <Welcome />
+      <Hello />
+      <Title name="Fronetend Developer" />
+      <Title name="Mobile Developer" />
+      <Title name="Solidity Developer" />
+      <Myname />
     </div>
   );
 }
