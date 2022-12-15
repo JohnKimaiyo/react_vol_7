5b4ff0c8e93cc43bb74ba26b456548be050bd317
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-14 23:19:33 -0800

    booklist component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from "react";
 import "./App.css";
-import Greet from "./component/Greet";
-import Welcome from "./component/Welcome";
-import Hello from "./component/Hello";
-import Title from "./component/Title";
-import Myname from "./component/Mycount";
-import Form from "./component/Form";
-import Users from "./component/Users";
-import Event from "./component/Event";
-import Range from "./component/Range";
-import Submit from "./component/Submit";
+import BookList from "./component/BookList";
+
+
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
-      <Hello />
-      <Title name="Fronetend Developer" />
-      <Title name="Mobile Developer" />
-      <Title name="Solidity Developer" />
-      <Myname />
-      <Form />
-      <Users />
-      <Range />
-      <Event />
-      <Submit/>
+      <BookList/>
     </div>
   );
 }
