380fd240cf03cfb5e407bbb826b0d0694bbcc1d1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-15 10:38:09 -0800

    the context

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import BookList from "./component/BookList";
 import Navbar from "./component/Navbar";
-
+import ThemeContextProvider from "./context/ThemeContextProvider";
 
 function App() {
   return (
     <div className="App">
-      <BookList/>
-      <Navbar/>
+      <ThemeContextProvider>
+        <BookList />
+        <Navbar />
+      </ThemeContextProvider>
     </div>
   );
 }
